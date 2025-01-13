78ebea169e63a40887ca9e789d787ca6998311db
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2025-01-13 13:15:09 +0300

    Update internal_evds.R

--- R/internal_evds.R
+++ R/internal_evds.R
@@ -1,10 +1,15 @@
 quick_check_evds <- function(key = "..") {
-  url <- create_evds_url("subject", key = key)
-  gelen <- request_httr2_helper_evds(url, cache = F)
-  if (!is_response(gelen)) {
-    return(false)
+  domain <- "https://evds2.tcmb.gov.tr/service/evds"
+
+  api_key_evds <- key
+  if (is.null(key)) {
+    api_key_evds <- get_api_key("evds")
   }
-  T
+
+  url <- glue::glue("{domain}/categories/type=json")
+  resp <- req_version_2_w_header(url, key = key)
+
+  return(is_response(resp))
 }
 mock_data_evds <- function() {
   dates <- seq(from = lubridate::ymd("2010-1-1"), to = lubridate::ymd("2025-1-1"), by = "month")
